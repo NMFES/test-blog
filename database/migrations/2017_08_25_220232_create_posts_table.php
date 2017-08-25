cb5e668fc442b9d889f5baac9b0528bddb725b70
Vitaliy S <j3ff3.me@gmail.com>
2017-08-26 02:35:43 +0300

    Create posts table and model

---
+++ database/migrations/2017_08_25_220232_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('title');
+            $table->integer('created_at', false, true);
+            $table->integer('updated_at', false, true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('posts');
+    }
+
+}
