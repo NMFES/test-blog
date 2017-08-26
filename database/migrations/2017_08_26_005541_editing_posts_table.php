22854885788e8c0e42e416dee82f3aa3e7e3e45e
Vitaliy S <j3ff3.me@gmail.com>
2017-08-26 04:31:34 +0300

    Editing posts table

---
+++ database/migrations/2017_08_26_005541_editing_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditingPostsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title')->change();
+            $table->text('description')->after('title');
+            $table->text('content')->after('description');
+            $table->string('meta_description')->after('content');
+            $table->string('meta_keywords')->after('meta_description');
+            $table->tinyInteger('status')->unsigned()->after('meta_keywords');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn(['description', 'text', 'meta_description', 'meta_keywords', 'status']);
+        });
+    }
+
+}
